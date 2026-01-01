b3e9c5e4dd194a24c9bc8c188c4678a313804101
vivekminipuri <vivekm2023@gmail.com>
2026-01-01 22:57:50 +0530

    Update firebase.ts

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFXxDqcABmRJYypSYINaygpxb2gQVDKLQ",
-    authDomain: "ai-fake-news-detector-79bfe.firebaseapp.com",
-    projectId: "ai-fake-news-detector-79bfe",
-    storageBucket: "ai-fake-news-detector-79bfe.firebasestorage.app",
-    messagingSenderId: "644548607516",
-    appId: "1:644548607516:web:def5e2cc5ae80942ab29d9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
